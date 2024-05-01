387959aaf26a8d14b9380c63688a65c9b8813eef
crrashh1542 <kotsuki@crrashh.com>
2024-05-02 01:47:53 +0800

    chore(config): 添加更多说明
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- config/config.ts
+++ config/config.ts
@@ -1,24 +1,37 @@
+/**
+ * config.ts 是咕咕主页的内容配置文件
+ * 按照注释上对键值对的解释，试着改改看吧！(=ﾟωﾟ)=
+ */
 export default {
+
     // config 配置文件版本
     configVersion: 1,
 
     // 网站基础信息
     site: {
+
         // 指定主页的名称 & 标题
         name: '云萧的咕咕客栈',
+
         // 指定主页运行的网址
         url: 'https://www.crrashh.com',
+
         // 指定主页的运行目录，一般保持默认即可
         base: '/',
+
         // 指定主页的资源文件 CDN，设置后需要将 ch_assets 文件夹下的资源传输到该 CDN 目录下
         // cdn: 'https://cdn.aaaaaa.bbb',
+
         // 指定主页的浏览器标题后缀
         suffix: ' / under building...',
+
         // 指定主页的副标题
         description: '以万象之不息，致不息之万象。',
+
         // 指定 <meta name="description" > 的 content 属性，用于展示给搜索引擎
         seoDescription: '基于Astro构建的咕咕的个人主页！云萧的咕咕客栈 / Creating makes miracles. / 以万象之不息，致不息之万象。'
     },
+
 
     // 顶栏导航
     nav: [
@@ -34,6 +47,7 @@ export default {
             url: 'https://blog.crrashh.com/archives/5'
         }
     ],
+
 
     // 横幅内的下侧社交媒体栏
     social: [
@@ -68,13 +82,16 @@ export default {
         }
     ],
 
+
     // banner 横幅下的由组件组成的内容区，内容顺序分先后
     components: {
 
         // 个人其他网站链接
         links: {
+
             // 链接组件的标题
             title: '咱的好东西！',
+
             to: [
                 // 此处的每一个对象（用大括号包起来的东西）对应一个内容区的灰色背景链接
                 //     其中 “title” 为第一行显示的粗体标题，“description” 为显示在标题下的
@@ -104,16 +121,22 @@ export default {
         }
     },
 
+
     // 页脚
     footer: {
+
         // 版权信息
         'copyright': '© Copyright 2022-2024 Kotsuki Crrashh. All Rights Reserved.',
+
         // 备案信息，如果不填则不在页脚显示相关内容
         'beian': {
+
             // 工信部备案号
             'miit': '蜀ICP备2022029657号-2',
+
             // gongan 备案号
             // 'gongan': null,
+
             // 非官方的 moe.gov.icp 号
             'moe': '萌ICP备20220551号'
         }
